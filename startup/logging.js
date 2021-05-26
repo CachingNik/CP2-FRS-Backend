a51b8a0eecdb4b5360ce07ce345821fcb4fbb042
Nikhil Gupta <ngupta_be17@thapar.edu>
2021-05-26 17:07:17 +0530

    logs (uncaught exceptions) can now be stored in MongoDB

--- startup/logging.js
+++ startup/logging.js
@@ -1,16 +1,24 @@
 const winston = require("winston");
 require("winston-mongodb");
 require("express-async-errors");
+const config = require("config");
 
 module.exports = function () {
   winston.exceptions.handle(
     new winston.transports.Console({ colorize: true, prettyPrint: true }),
-    new winston.transports.File({ filename: "uncaughtExceptions.log" })
+    new winston.transports.File({ filename: "uncaughtExceptions.log" }),
+    new winston.transports.MongoDB({
+      db: config.get("db"),
+      options: { useUnifiedTopology: true },
+    })
   );
 
   process.on("unhandledRejection", (ex) => {
     throw ex;
   });
 
-  winston.add(new winston.transports.File({ filename: "logfile.log" }));
+  winston.add(
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
+    new winston.transports.File({ filename: "logfile.log" })
+  );
 };
