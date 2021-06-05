4183d1e118c3a862547c0c1f511b551c46cc49c8
Nikhil Gupta <ngupta_be17@thapar.edu>
2021-06-05 14:37:38 +0530

    Added service for sending mails & also validation of passenger info for flight booking

--- models/order.js
+++ models/order.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
+const moment = require("moment");
 
 const genders = ["male", "female"];
 
@@ -34,8 +36,42 @@ const orderSchema = new mongoose.Schema({
     ],
     required: true,
   },
+  amount: { type: Number, required: true },
 });
 
 const Order = mongoose.model("Order", orderSchema);
 
+function validateOrder(order) {
+  const schema = Joi.object({
+    flightId: Joi.objectId().required(),
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    mobileNumber: Joi.number()
+      .integer()
+      .min(1000000000)
+      .max(9999999999)
+      .required(),
+    adultList: Joi.array().items({
+      gender: Joi.string().valid("male", "female").required(),
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+    }),
+    childList: Joi.array().items({
+      gender: Joi.string().valid("male", "female").required(),
+      firstName: Joi.string().required(),
+      lastName: Joi.string().required(),
+      dateOfBirth: Joi.date()
+        .min(moment().subtract(11, "years").format("YYYY-MM-DD"))
+        .required()
+        .messages({
+          "date.min": "Age of child must be less than or equal to 11.",
+        }),
+    }),
+    amount: Joi.number().required(),
+  });
+
+  return schema.validate(order);
+}
+
 exports.Order = Order;
+exports.validate = validateOrder;
--- startup/config.js
+++ startup/config.js
@@ -12,4 +12,12 @@ module.exports = function () {
   if (!config.get("razorpaySecret")) {
     throw new Error("FATAL ERROR: razorpaySecret is not defined.");
   }
+
+  if (!config.get("email")) {
+    throw new Error("FATAL ERROR: email is not defined.");
+  }
+
+  if (!config.get("password")) {
+    throw new Error("FATAL ERROR: password is not defined.");
+  }
 };
